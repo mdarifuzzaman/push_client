41be9cfca16f6d2de59b878325111d4c853743f2
Md Arifuzzaman <arif_uap@outlook.com>
2021-08-04 21:52:53 +1000

    update sw

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
 
-self.addEventListener('push', () => {
-    console.log(self.registration);
-    self.registration.sendNotification('Hoxro sends a notification', {});
+self.addEventListener('push', (e) => {
+    var options = {
+        body: 'This notification was generated from hoxro!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {action: 'explore', title: 'Explore the hoxro system', icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close', icon: 'images/xmark.png'}
+        ]        
+    };
+
+    e.waitUntil(self.registration.showNotification('Hello world', options));
+
 });
