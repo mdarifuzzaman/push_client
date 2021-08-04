a6b52dd47afb9b930c3ef1fa9b7e086fd3311057
Md Arifuzzaman <arif_uap@outlook.com>
2021-08-04 22:33:18 +1000

    update

--- push.js
+++ push.js
@@ -7,4 +7,4 @@ let vapidkeys = {
 
   push.setVapidDetails('mailto:test@hoxro.co.uk', vapidkeys.publicKey, vapidkeys.privateKey);
   let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fq1M18CzvHQ:APA91bGFAmw0v1KLEo5T4OzJ4suiyrtUH4fwv6RKqtVOQ6FKK137bWGkZlp5_ee7PVs56N7JZdeqGQNHyioPr-TuGzZlUrza_hi6ziwecugx8WFfYLeL8DHeVgPah00MRXhgy_5wrll0","expirationTime":null,"keys":{"p256dh":"BDMjqVmc8X9UF0V5CNEr-KaSEiW28SXFU2fro9U0arnzIxvSbJIiL1NPG_8OIE-4RRbyyfmIWkwtNrbMsnjAeJQ","auth":"_hkCj9IoKVYRdLtkKAd4iA"}};
-  push.sendNotification(sub, 'Hellow from matter');
+  push.sendNotification(sub, '{"title": "hello there", "message": "hey how are you"}');
