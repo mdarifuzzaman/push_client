eab631f20d6f2ffd1f7f9bf407dcbd8a0f26fe2f
Md Arifuzzaman <arif_uap@outlook.com>
2021-08-05 16:56:24 +1000

    update the code

--- push.js
+++ push.js
@@ -6,5 +6,5 @@ let vapidkeys = {
   };
 
   push.setVapidDetails('mailto:test@hoxro.co.uk', vapidkeys.publicKey, vapidkeys.privateKey);
-  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fq1M18CzvHQ:APA91bGFAmw0v1KLEo5T4OzJ4suiyrtUH4fwv6RKqtVOQ6FKK137bWGkZlp5_ee7PVs56N7JZdeqGQNHyioPr-TuGzZlUrza_hi6ziwecugx8WFfYLeL8DHeVgPah00MRXhgy_5wrll0","expirationTime":null,"keys":{"p256dh":"BDMjqVmc8X9UF0V5CNEr-KaSEiW28SXFU2fro9U0arnzIxvSbJIiL1NPG_8OIE-4RRbyyfmIWkwtNrbMsnjAeJQ","auth":"_hkCj9IoKVYRdLtkKAd4iA"}};
-  push.sendNotification(sub, '{"title": "hello there", "message": "hey how are you"}');
+  let sub = {"endpoint":"https://wns2-sg2p.notify.windows.com/w/?token=BQYAAACV2oJaey5aFSgyPfPlArPsqmLlTrZ%2fNg%2fpp5fzYvZ2wmjfaX4ug%2fgsFd%2bVr2mHDuc3VaWKW1UACrwQ4jR8qmKl9hUyQmPqYfIELTKHPIup%2bH9ebAdI5NfrP7xxqRXCHdymKcfIn3D%2bwLKWPH8ildkFuQF8PI7wwPkY6ZD4LQp4snrqQMfqNSkQLlyYycIqT2oiZ%2b5sW9OHenF0Aeob0qvfnfxAS89lpnwvRQJb6pV78AT%2bLkyywEmdYcG22iPG7EcxtI7E5%2fdpM9M2IPl2qMy0%2bAZIEnNyXl0XpbHJpssX%2fUYCNLzd5OXSpcdakf4NE56m6aktW7UF5OsGXcQorC0n","expirationTime":null,"keys":{"p256dh":"BNyYmSmN4oSMu-L2XLbxnhCZjUIleqeBzb3-9VOkLED2ENAVcAjAOmdcdz9-FLbTt7eCrFQN18uKNMsxH_iLO9U","auth":"z7wL7Y7UBblAI98OEyiaJg"}};
+  push.sendNotification(sub, '{"title": "from hoxro", "message": "A matter is created."}');
